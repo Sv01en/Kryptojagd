f58868348e645fd66aa8b8ad75ed253ed4336450
mickmini <95034077+mickmini@users.noreply.github.com>
2021-12-13 18:06:18 +0100

    comment redundant classes
    we are not sure yet if we will use them, because we have not decided yet how to implement it

--- src/main/java/org/kryptojagd/encryptionmethods/Encryption.java
+++ src/main/java/org/kryptojagd/encryptionmethods/Encryption.java
@@ -1,5 +1,6 @@
 package org.kryptojagd.encryptionmethods;
 
+//redundant class, because we are not sure if we use interface or abstract class
 public abstract class Encryption {
 
     static String encode(String text, String key) {
--- src/main/java/org/kryptojagd/level/DecryptionTask.java
+++ src/main/java/org/kryptojagd/level/DecryptionTask.java
@@ -2,6 +2,7 @@ package org.kryptojagd.level;
 
 import java.util.HashMap;
 
+//redundant class, because we are not sure how to use json
 public class DecryptionTask {
 
 	public DecryptionTask(HashMap<String,Object> map) {
