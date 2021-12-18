d0b9e410b4d218b1d88774a591f9054c3661dd67
mickmini <95034077+mickmini@users.noreply.github.com>
2021-12-18 22:33:26 +0100

    created LevelController
    to have a class which handles the tasks of a level

---
+++ src/main/java/org/kryptojagd/controls/LevelController.java
@@ -0,0 +1,22 @@
+package org.kryptojagd.controls;
+
+import org.kryptojagd.level.Level;
+
+/**
+ * The class handles a level in the game
+ *
+ * @author Sonja
+ */
+public class LevelController {
+
+    private Level level;
+
+    public LevelController(Level level) {
+        this.level = level;
+    }
+
+    public void runMultipleChoice(){
+
+    }
+
+}
