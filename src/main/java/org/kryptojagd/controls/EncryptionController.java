a367652798408278ecc5b32c9b7a5d752d069415
Sv01en <sven@privat-strasser.de>
2022-01-11 17:07:11 +0100

    Timer in EncryptionController implemented

--- src/main/java/org/kryptojagd/controls/EncryptionController.java
+++ src/main/java/org/kryptojagd/controls/EncryptionController.java
@@ -1,13 +1,17 @@
 package org.kryptojagd.controls;
 
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.util.Duration;
 import org.kryptojagd.encryptionmethods.Backwards;
 import org.kryptojagd.level.Level;
+import org.kryptojagd.level.countdown.CountdownTimer;
 
 /**
  * Encryption Controller for the corresponding fxml-file
@@ -16,6 +20,11 @@ import org.kryptojagd.level.Level;
  * @version 1.0
  */
 public class EncryptionController extends AbstractController {
+
+    private CountdownTimer countdownTimer;
+
+    @FXML
+    private Label timer = new Label();
 
     @FXML
     private Button button1;
@@ -31,6 +40,13 @@ public class EncryptionController extends AbstractController {
 
     private final Level level = mainController.getCurrentLevel();
 
+    @FXML
+    public void initialize() {
+        label1.setText(level.getEncryptionTask().getTask());
+        label2.setText(level.getEncryptionTask().getText());
+        this.countdownTimer = new CountdownTimer(level.getTimeInSec());
+        updateTimer();
+    }
 
     @FXML
     void checkEncryption(ActionEvent event) {
@@ -43,9 +59,24 @@ public class EncryptionController extends AbstractController {
             System.out.println("Probiere es nochmal");
         }
     }
-    public void initialize() {
-        label1.setText(level.getEncryptionTask().getTask());
-        label2.setText(level.getEncryptionTask().getText());
-    }
 
+    /**
+     * Updates the {@link EncryptionController#timer} every second in the corresponding fxml-file.
+     */
+    @FXML
+    void updateTimer() {
+        Timeline time = new Timeline();
+        time.setCycleCount(Timeline.INDEFINITE);
+        time.stop();
+        KeyFrame frame = new KeyFrame(Duration.seconds(1), actionEvent -> {
+            timer.setText(countdownTimer.getCurrentValue());
+            mainController.getCurrentLevel().setTimeInSec(Integer.parseInt(countdownTimer.getCurrentValue()));
+            if (Integer.parseInt(countdownTimer.getCurrentValue()) == 0) {
+                mainController.switchWindow("Decryption.fxml");
+                time.stop();
+            }
+        });
+        time.getKeyFrames().add(frame);
+        time.playFromStart();
+    }
 }
