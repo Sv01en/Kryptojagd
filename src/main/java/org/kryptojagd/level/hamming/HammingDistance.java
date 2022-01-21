388e26467d0ecb1a2a43e435b3b48094f3dc8df0
Sv01en <sven@privat-strasser.de>
2022-01-21 10:03:09 +0100

    Implemented hamming distance

---
+++ src/main/java/org/kryptojagd/level/hamming/HammingDistance.java
@@ -0,0 +1,32 @@
+package org.kryptojagd.level.hamming;
+
+/**
+ * Provides all functionalities for the hamming distance.
+ * @author Sven Strasser
+ * @version 1.0
+ */
+public class HammingDistance {
+
+    /**
+     * Calculates the hamming distance between two given words.
+     * @param first given word as a string
+     * @param second given word as a string
+     * @return hamming distance as an integer
+     */
+    public int calculateHammingDistance(String first, String second) {
+
+        char[] s1 = first.toCharArray();
+        char[] s2 = second.toCharArray();
+
+        int shorter = Math.min(s1.length, s2.length);
+        int longest = Math.max(s1.length, s2.length);
+
+        int counter = 0;
+        for (int i=0; i<shorter; i++) {
+            if (s1[i] != s2[i]) counter++;
+        }
+        counter += longest - shorter;
+
+        return counter;
+    }
+}
