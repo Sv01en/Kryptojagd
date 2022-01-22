ce76db713f720e460d066935c75e3a94975fc4a0
Leah Schlimm <leah.schlimm@student.kit.edu>
2022-01-22 01:23:40 +0100

    update naming of buttons and methods

--- src/main/java/org/kryptojagd/controls/LaunchWindowController.java
+++ src/main/java/org/kryptojagd/controls/LaunchWindowController.java
@@ -10,22 +10,13 @@ import javafx.scene.control.Button;
  * @author Leah Schlimm
  */
 public class LaunchWindowController extends AbstractController {
-    
-    @FXML
-    private Button startButton;
-
-    @FXML
-    private Button infoButton;
-
-    @FXML
-    private Button einstellungenButton;
 
     /**
      * Handles press on settings button
      * @param event that is received
      */
     @FXML
-    void klickEinstellungen(ActionEvent event) {
+    void clickSettings(ActionEvent event) {
         System.out.println("Das ist ein Test");
         //mainController.switchWindowWithCSS("Encryption.fxml", "../css/startwindow.css");
     }
@@ -35,7 +26,7 @@ public class LaunchWindowController extends AbstractController {
      * @param event that is received
      */
     @FXML
-    void klickInfo(ActionEvent event) {
+    void clickInfo(ActionEvent event) {
     	System.out.println("Es wurde auf Info geklickt!");
         mainController.switchWindowWithCSS("Infotext.fxml", "../css/startwindow.css");
     	//mainController.switchWindow("Infotext.fxml");
@@ -46,7 +37,7 @@ public class LaunchWindowController extends AbstractController {
      * @param event that is received
      */
     @FXML
-    void klickStart(ActionEvent event) {
+    void clickStart(ActionEvent event) {
     	System.out.println("Es wurde auf Start geklickt!");
     	mainController.runLevel();
     }
@@ -61,7 +52,7 @@ public class LaunchWindowController extends AbstractController {
      * @param event that is received
      */
     @FXML
-    public void klickLevelSelector(ActionEvent event) {
+    public void clickLevelSelector(ActionEvent event) {
         mainController.switchWindowWithCSS("LevelSelector.fxml", "../css/startwindow.css");
     }
 }
