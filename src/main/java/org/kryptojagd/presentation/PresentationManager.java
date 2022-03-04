854a0d18ea6730d46b08ff16bc4cd6893cdbcb72
mickmini <95034077+mickmini@users.noreply.github.com>
2022-03-04 20:58:59 +0100

    changed every version to 11, and created an artifact

--- src/main/java/org/kryptojagd/presentation/PresentationManager.java
+++ src/main/java/org/kryptojagd/presentation/PresentationManager.java
@@ -7,7 +7,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
